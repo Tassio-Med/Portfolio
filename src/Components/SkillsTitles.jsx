cb78503ec2274d0f55e7f4b549431dd45519a8d8
Tassio-Med <tassiohfmed@gmail.como>
2023-10-26 17:18:06 -0300

    feat: animação de scroll velocity adicionada

---
+++ src/Components/SkillsTitles.jsx
@@ -0,0 +1,76 @@
+import "../Assets/Styles/index.css";
+import { useRef } from "react";
+import { motion, useScroll, useSpring, useTransform, useMotionValue, useVelocity, useAnimationFrame } from "framer-motion";
+import { wrap } from "@motionone/utils";
+import PropTypes from "prop-types"; // Importe a biblioteca prop-types
+
+function SkillsTitle({ children, baseVelocity }) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+  const directionFactor = useRef(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  return (
+    <div className="fixedd w-full overflow-hidden letter-spacing[-2px] leading-0.8 whitespace-nowrap flex flex-wrap-nowrap justify-center items-center">
+      <motion.div
+        className="flex font-semibold text-uppercase text-4xl md:text-9xl whitespace-nowrap z-0"
+        style={{ x }}
+      >
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block mr-8">{children} </span>
+        <span className="block">{children} </span>
+      </motion.div>
+    </div>
+  );
+}
+
+// Adicione as validações de props usando PropTypes
+SkillsTitle.propTypes = {
+  children: PropTypes.string.isRequired,
+  baseVelocity: PropTypes.number,
+};
+
+
+export default SkillsTitle;
