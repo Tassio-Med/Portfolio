f8e785dea19f182fcac4296198b71f59372d5d1f
Tassio-Med <tassiohfmed@gmail.como>
2023-10-27 12:07:47 -0300

    feat: caminho para os links atualizado

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
 
 
   return (
-    <main className={styles.main}>
+    <main className={styles.main} id="home">
       <motion.div 
         className={styles.mask}
         animate={{
