c8fee6c9860dac086a15308c34f5f09482cc55a1
Tassio-Med <tassiohfmed@gmail.como>
2023-10-31 12:15:01 -0300

    feat: elemento alinhado

--- src/Components/MobileWarning.jsx
+++ src/Components/MobileWarning.jsx
@@ -1,36 +1,9 @@
-// import PropTypes from "prop-types";
-
-// const MobileWarning = ({ onClose }) => {
-//   return (
-//     <div className="fixed top-0 px-10 left-0 w-full h-full bg-neutral-900 bg-opacity-95 flex items-center justify-center z-[200]">
-//       <div id="responsive" className="bg-white p-6 rounded-lg w-full sm:w-[80%] md:w-[60%] lg:w-[40%] flex flex-col text-center">
-//         <p className="text-gray-800">
-//           Sinto muito, o site ainda não é compatível com dispositivos móveis.  <br /> Estou trabalhando nisso. 🔧📱
-//         </p>
-//         <p className="text-gray-800">Recomendo acessá-lo a partir de um computador.</p>
-//         <button
-//           onClick={onClose}
-//           className="mt-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 cursor-pointer"
-//         >
-//           Fechar
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// MobileWarning.propTypes = {
-//   onClose: PropTypes.func.isRequired,
-// };
-
-// export default MobileWarning;
-
 import PropTypes from "prop-types";
 
 const MobileWarning = ({ onClose }) => {
   return (
-    <div className="fixed top-0 px-10 left-0 w-full h-full bg-neutral-900 bg-opacity-95 flex items-center justify-center z-[200]">
-      <div className="w-full flex justify-center items-center">
+    <div className="fixed top-0 px-10  w-full h-screen bg-neutral-900 bg-opacity-95 flex justify-center items-center z-[200]">
+      <div id="element" className="w-full h-screen mr-4 flex justify-center items-center">
         <div
           id="responsive"
           className="bg-white p-6 rounded-lg w-full sm:w-[80%] md:w-[60%] lg:w-[40%]  text-center"
